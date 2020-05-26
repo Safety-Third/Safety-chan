f7de91501ad3def80383b6adc547cfc890bfd822
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2020-05-26 16:38:51 -0400

    [feature] add categories to stats

--- bot/cogs/stats.py
+++ bot/cogs/stats.py
@@ -1,9 +1,10 @@
 from discord import Guild, Member, TextChannel, User
-from discord.ext.commands import Bot, Context, command, CommandInvokeError, guild_only
+from discord.ext.commands import Bot, Context, check_any, command, \
+CommandInvokeError, guild_only, has_permissions, is_owner
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
 from .base import CustomCog
-from ..util import redis
+from ..util import redis, redlocks
 
 __all__ = ["StatsManager"]
 
@@ -28,6 +29,25 @@ class StatsManager(CustomCog):
   def __init__(self, bot: Bot):
     self.bot = bot
 
+  def get_guild(self, idOrName: Union[int, str]) -> Optional[Guild]:
+    """
+    Function for finding a guild using its id or name
+    
+    Args:
+      idOrName: a possible server name or id
+
+    Returns:
+      a guild whose name matches the name or whose id matches the id if exists, or None
+    """
+    if isinstance(idOrName, int):
+      return self.bot.get_guild(idOrName)
+    else:
+      for guild in self.bot.guilds:
+        if guild.name == idOrName:
+          return guild
+
+    return None
+
   def get_guild_key(self, idOrName: Union[int, str], ctx: Context) -> Optional[GuildAndKey]:
     """
     Function for extracting the guild name and key from user-provided id/name
@@ -39,15 +59,7 @@ class StatsManager(CustomCog):
     Returns:
       a Tuple repesenting the guild name and key, if such a guild (name or id) exists
     """
-    user_guild: Optional[Guild] = None
-
-    if isinstance(idOrName, int):
-      user_guild = self.bot.get_guild(idOrName)
-    else:
-      for guild in self.bot.guilds:
-        if guild.name == idOrName:
-          user_guild = guild
-          break
+    user_guild = self.get_guild(idOrName)
 
     if user_guild:
       key = f"{ctx.author.id}:{user_guild.id}"
@@ -56,7 +68,7 @@ class StatsManager(CustomCog):
       return None
 
   async def handle_message(self, ctx: Context, idOrName: GuildIdOrNumber, 
-                          handler: Callable[[str], None]): 
+                          handler: Callable[[str], str]): 
     """
     Generic function for handling messages from a user
 
@@ -92,6 +104,92 @@ class StatsManager(CustomCog):
         raise ValueError("You must provide a guild id/name to consent. Alternatively, you can message in a server channel")
 
     await ctx.author.send(f"{message} in {guildName}")
+
+  @command()
+  async def categories(self, ctx: Context, max_per_category = 5, idOrName: GuildIdOrNumber = None):
+    """
+    Get stats of your emoji usage in a guild, by category. These stats are DMed.
+
+    By default, will send you the top 5 emojis per section (up to 2x max_per_category, if tied).
+    You can change the number of emojis by providing a number as your first argument.
+    Regardless, you will get a total count of the emojis per section.
+
+    This can be called in a server to get stats for that server, 
+    or you can provide a server ID/name via a DM. 
+    You have to provide an emoji count in this case.
+
+    Examples:
+    >categories                        (stats in server text channel)
+    >categories 20                     (show the top 20 emojis per category)
+    >categories 5 000000000000000000   (show top 5 emojis by category using server id)
+    >categories 10 "test server"       (show top 10 emojis by category using server name)
+    """
+    def handler(key: str):
+      react_stats = redis.hgetall(key)
+
+      guild_id = key[key.index(":") + 1:]
+      categories = redis.hgetall(f"{guild_id}:categories")
+      
+      if len(react_stats) > 1 and categories:
+        message = ">>> "
+
+        score_mapping_by_category: Dict[Optional[str], Dict[int, List[str]]] = {}
+
+        for [emoji, count] in react_stats.items():
+          if emoji != "consent":
+            category = None
+
+            for [key, emojilist] in categories.items():
+              if emoji in emojilist:
+                category = key
+                break
+
+            category_map = score_mapping_by_category.get(category, {})
+
+            int_score = int(count)
+
+            if int_score in category_map:
+              category_map[int_score].append(emoji)
+            else:
+              category_map[int_score] = [emoji]
+
+            score_mapping_by_category[category] = category_map
+
+        section_and_top_emojis: List[Tuple[int, List[str], Optional[str]]] = []
+
+        for [category, mapping] in score_mapping_by_category.items():
+          emoji_count = 0
+          top_emojis: List[str] = []
+          total_count = 0
+
+          for count in sorted(mapping, reverse=True):
+            emojis = mapping[count]
+
+            if emoji_count < max_per_category and emoji_count + len(emojis) < max_per_category * 2:
+              top_emojis.append("  ".join(emojis) + f" ({count})")
+            
+            emoji_count += len(emojis)
+            total_count += count * len(emojis)
+
+          section_and_top_emojis.append([total_count, top_emojis, category])
+
+        for [total_count, emojis, category] in sorted(section_and_top_emojis, reverse=True):
+          emojis_joined = "  ".join(emojis)
+
+          if category is None:
+            message += "**No category**"
+          else:
+            message += category
+
+          message += f" ({total_count} uses): {emojis_joined}\n"
+
+        return message
+      else:
+        message = "You have used no emojis"
+
+      return message
+
+    await self.handle_message(ctx, idOrName, handler)
 
   @command()
   async def consent(self, ctx: Context, idOrName: GuildIdOrNumber = None):
@@ -193,6 +291,9 @@ class StatsManager(CustomCog):
         for count in sorted(score_mappings, reverse=True):
           emojis = score_mappings[count]
 
+          if len(emojis) + emoji_count >= maxEmojis * 2:
+            break
+
           if count == 1:
             message += "1 use: "
           else:
@@ -213,6 +314,57 @@ class StatsManager(CustomCog):
 
     await self.handle_message(ctx, idOrName, handler)
   
+  @check_any(has_permissions(manage_emojis=True), is_owner())
+  @command()
+  async def setCategory(self, ctx: Context, category: str, *emojis):
+    """
+    Sets a list of emojis to a specific category.
+    This category will be used to determine which "categories" are most used by a specific person.
+    An emoji can only belong to a single category.
+
+    If you want to specify which server to use, provide the server id or name
+    as the last argument.
+
+    Examples:
+    >setCategory joy :three:                      (sets the category 'joy' to be [3])
+    >setCategory joy :three: :four: :five:        (sets the category 'joy' to be [3, 4, 5])
+    >setCategory joy :three: :four: "test server" (sets the category 'joy' in "test server" to [3])
+    >setCategory joy :three: 000000000000000000   (sets the category 'joy' in server with id
+                                                  000000000000000000  to [3])
+    """
+    if len(emojis) == 0:
+      raise ValueError("You must provide at least one emoji")
+
+    guildId = ""
+
+    guild = self.get_guild(emojis[-1])
+
+    if guild is None:
+      if isinstance(ctx.channel, TextChannel):
+        guildId = ctx.channel.guild.id
+      else:
+        raise ValueError(f"Could not find a server {emojis[-1]}. If you are DM-ing, make sure to provide the server name/id as the last argument")
+    else:
+      guildId = guild.id
+      emojis = emojis[:-1]
+        
+    key = f"{guildId}:categories"
+
+    with redlocks.create_lock(f"{key}:lock"):
+      existing_categories = redis.hgetall(key)
+
+      for [existing_category, emojilist] in existing_categories.items():
+        if existing_category == category:
+          continue
+
+        for emoji in emojis:
+          if emoji in emojilist:
+            raise ValueError(f"Emoji {emoji} is already used in category {existing_category}")
+
+      redis.hmset(key, { category: " ".join(emojis)})
+
+    await ctx.send(f"{ctx.author.mention} set category {category} to {' '.join(emojis)}")
+
   @command()
   async def uses(self, ctx: Context, *emojis):
     """
@@ -258,3 +410,39 @@ class StatsManager(CustomCog):
       return message
 
     await self.handle_message(ctx, idOrName, handler)
+
+  @command()
+  async def viewCategories(self, ctx: Context, idOrName: GuildIdOrNumber = None):
+    """
+    View the emoji categories in a specific server.
+
+    You can provide a server id or name to specify a certain server to use, or
+    send the message in a server (no arguments) to get the categories for that server.
+
+    Examples:
+    >viewCategories                     (view emoji categories in current server)
+    >viewCategories "test server"       (view emoji categories in server "test server")
+    >viewCategories 000000000000000000  (view emoji categories in server with id 000000000000000000)
+    """
+    guild: Optional[Guild] = None    
+
+    if idOrName:
+      guild = self.get_guild(idOrName)
+    else:
+      if isinstance(ctx.channel, TextChannel):
+        guild = ctx.channel.guild
+
+    if guild is None:
+      raise ValueError(f"Could not find a server {idOrName}. If you are DM-ing, make sure to provide the server name/id as the last argument")
+      
+    categories = redis.hgetall(f"{guild.id}:categories")
+
+    if categories:
+      message = f">>> Emoji categories in {guild.name}:"
+
+      for [category, emojilist] in categories.items():
+        message += f"\n{category}: {emojilist}"
+
+      await ctx.send(message)
+    else:
+      await ctx.send(f"No categories for server {guild.name}")
